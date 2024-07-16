cf90571d1535d3d55322670883ccda031d2fb3a3
Rafael Pertile <rafaelpertile@gmail.com>
2024-07-15 22:05:25 -0300

    Atualizado o sistema do BluDV

--- bludv.js
+++ bludv.js
@@ -1,8 +1,21 @@
-
 {
-	window.location.href = document.getElementsByTagName('a')[0];
-	window.location.href = document.getElementById('downpart2').getElementsByTagName('a')[0].href;
-	/*
+  /* Expressão regular para capturar o link magnet*/
+  const magnet = document.getElementsByName("link")[0].value;
+  close();
+  const newWindow = window.open(magnet);
+  if (newWindow) {
+    // Aguardar um pouco para garantir que a nova janela foi carregada
+    setTimeout(() => {
+      // Fechar a nova janela após um pequeno atraso
+      newWindow.close();
+    }, 5000); // 5000 milissegundos = 5 segundos
+  }
+  /*Códigos antigos abaixo
+  window.location.href = document.getElementsByTagName("a")[0];
+  window.location.href = document
+    .getElementById("downpart2")
+    .getElementsByTagName("a")[0].href;
+  
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 		alert("aq2");
 		if (changeInfo.status == 'complete' && myURLs.some(url => tab.url.includes(url))) {
